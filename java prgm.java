698de86022795ac0f1131918ade0dc10b4dfe8f7
yukta2504 <22h51a04k9@cmrcet.ac.in>
2024-10-26 15:48:19 +0530

    Add files via upload

---
+++ java prgm.java
@@ -0,0 +1,131 @@
+
+
+//even and odd
+//class  test{
+//    public static void main(String[] args) {
+//      int a=1123;
+//    if(a%2 == 0)
+//  System.out.println("it is even");
+//else
+//System.out.println("it is odd");
+//}
+//}
+
+import java.util.Scanner;
+//leap year
+/*class test{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%4==0)
+        System.out.println("leap year");
+        else if(a%100==0)
+        System.out.println("leap year");
+        else if(a%400==0)
+        System.out.println("leap year");
+        else 
+        System.out.println("not leap year");
+    }
+}*/
+
+
+//greatest number
+/*class test{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if(a>b && a>c)
+        System.out.println("a is greater");
+        else if(b>a && b>c)
+        System.out.println("b is greater");
+        else 
+        System.out.println("c is greater");
+    }
+}*/
+
+//range b/w numbers
+// class test{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         if (a>=1 && a<=50)
+//         System.out.println("range b/w 1 to 50");
+//         else if(a>=51 && a<=100)
+//         System.out.println("range b/w 51 to 100");
+//         else if(a>=101&&a<=150)
+//         System.out.println("range b/w 101 to 150");
+//         else if(a>=151&&a<=200)
+//         System.out.println("range b/w 151 to 200");
+//         else
+//         System.out.println("range is not specified");
+//     }
+// }
+
+//percentage of exam
+// class test{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         if (a>=90)
+//          System.out.println("grade A");
+//          else if(a>=75 && a<=89)
+//          System.out.println("grade B");
+//          else if(a>=65&&a<=74)
+//          System.out.println("grade C");
+//          else
+//          System.out.println("fail");
+//      }
+// }
+
+//perfect square
+// class test{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         double x=Math.sqrt(a);
+//         if (x*x==a)
+//         System.out.println("perfect");
+//         else
+//         System.out.println("not perfect");
+//     }
+// }
+ 
+
+//switch case
+// class test{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         switch (a) {
+//             case 1:
+//             System.out.println("one");
+//                 break;
+//             case 2:
+//                 System.out.println("two");
+//                     break;
+        
+//             default:
+//                 break;
+//         }
+//     }
+// }
+
+ class test{
+          public static void main(String[]args){
+              Scanner sc=new Scanner(System.in);
+              int a=sc.nextInt();
+              switch (a) {
+                  case 1:
+                  System.out.println("january");
+                      break;
+                  case 2:
+                      System.out.println("febuary");
+                          break;
+            
+                  default:
+                      break;
+              }
+          }
+    }
